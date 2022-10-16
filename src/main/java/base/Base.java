4df8fbc907b79e820c8fc294262f28a826ea9816
unknown <hasib8nh@gmail.com>
2022-10-16 15:52:07 +0600

    Run IndexPageTest

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -1,6 +1,7 @@
 package base;
 
 import java.io.FileInputStream;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
@@ -9,22 +10,25 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.BeforeTest;
 
+import actionDriver.Action;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Base {
 	public static Properties prop;
 	public static WebDriver driver;
+	
 	@BeforeTest
 	public void loadConfig() {
-//		ExtentManager.setExtent();
-//		DOMConfigurator.configure("log4j.xml");
-		
+		//ExtentManager.setExtent();
+		//DOMConfigurator.configure("log4j.xml");
+
 		try {
 			prop = new Properties();
 			FileInputStream ip = new FileInputStream(
-					System.getProperty("C:\\Program Files\\ecomp\\configuration\\config.properties"));
+					System.getProperty("user.dir") + "\\configuration\\config.properties");
 			prop.load(ip);
 
 		} catch (FileNotFoundException e) {
@@ -41,17 +45,34 @@ public void launchApp() {
 		//if (browserName.equalsIgnoreCase("Chrome")) {
 			WebDriverManager.chromedriver().setup();
 			String browserName = prop.getProperty("browser");
+			
+			if (browserName.equalsIgnoreCase("Chrome")) {
+				WebDriverManager.chromedriver().setup();
+				// Set Browser to ThreadLocalMap
+				driver = new ChromeDriver();
+			} else if (browserName.equalsIgnoreCase("FireFox")) {
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+			} else if (browserName.equalsIgnoreCase("IE")) {
+				WebDriverManager.iedriver().setup();
+				driver = new InternetExplorerDriver();
+			}
+			driver.manage().window().maximize();
+			Action.implicitWait(driver, 10);
+			Action.pageLoadTimeOut(driver, 20);
+			driver.get(prop.getProperty("url"));
+
 
 			// Set Browser to ThreadLocalMap
 			//driver.set(new ChromeDriver());
-		if (browserName.contains("Chrome")) {
-			//WebDriverManager.firefoxdriver().setup();
-			driver=new ChromeDriver();
-		} else if (browserName.contains("Firefox")) {
-			driver=new FirefoxDriver();
-			//driver.set(new InternetExplorerDriver());
-		}
-//		//Maximize the screen
+//		if (browserName.contains("Chrome")) {
+//			//WebDriverManager.firefoxdriver().setup();
+//			driver=new ChromeDriver();
+//		} else if (browserName.contains("Firefox")) {
+//			driver=new FirefoxDriver();
+//			//driver.set(new InternetExplorerDriver());
+//		}
+////		//Maximize the screen
 //		getDriver().manage().window().maximize();
 //		//Delete all the cookies
 //		getDriver().manage().deleteAllCookies();
---
+++ src/test/java/testcases/IndexPageTest.java
@@ -0,0 +1,45 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import base.Base;
+import pageObject.IndexPage;
+
+public class IndexPageTest extends Base {
+	private IndexPage indexPage;
+	
+	@BeforeMethod
+	public void setup() {
+		launchApp();
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void verifyLogo() throws Throwable {
+		//Log.startTestCase("verifyLogo");
+		indexPage= new IndexPage();
+		boolean result=indexPage.validateLogo();
+		Assert.assertTrue(result);
+		//Log.endTestCase("verifyLogo");
+	}
+	
+	@Test
+	public void verifyTitle() {
+		//Log.startTestCase("verifyTitle");
+		String actTitle=indexPage.getMyStoreTitle();
+		Assert.assertEquals(actTitle, "My Store");
+		//Log.endTestCase("verifyTitle");
+	}
+
+	
+	
+
+
+}
