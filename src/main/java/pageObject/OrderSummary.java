f63d817e61b06df65400aaffd0a39200157ad3a4
unknown <hasib8nh@gmail.com>
2022-10-16 13:55:04 +0600

    add properties in OrderSummary Page

--- src/main/java/pageObject/OrderSummary.java
+++ src/main/java/pageObject/OrderSummary.java
@@ -1,5 +1,25 @@
 package pageObject;
 
-public class OrderSummary {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import actionDriver.Action;
+import base.Base;
+
+public class OrderSummary extends Base {
+	
+	@FindBy(xpath="//span[contains(text(),'I confirm my order')]")
+	private WebElement confirmOrderBtn;
+	
+	public OrderSummary() {
+		PageFactory.initElements(driver, this);
+	}
+
+	public OrderConfPage clickOnconfirmOrderBtn() throws Throwable {
+		Action.click(driver, confirmOrderBtn);
+		return new OrderConfPage();
+	}
+	
 
 }
