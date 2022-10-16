78c550d292c026cd06654200c9b46cafbf117784
unknown <hasib8nh@gmail.com>
2022-10-16 14:00:50 +0600

    Add properties to OrderConfPage

--- src/main/java/pageObject/OrderConfPage.java
+++ src/main/java/pageObject/OrderConfPage.java
@@ -1,5 +1,26 @@
 package pageObject;
 
-public class OrderConfPage {
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+import base.Base;
+
+public class OrderConfPage extends Base {
+	
+	@FindBy(xpath="//p/strong[contains(text(),'Your order on My Store is complete.')]")
+	private WebElement confirmMessag;
+	
+	public OrderConfPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public String validateConfirmMessage() {
+		String confirmMsg=confirmMessag.getText();
+		return confirmMsg;
+	}
+
 
 }
