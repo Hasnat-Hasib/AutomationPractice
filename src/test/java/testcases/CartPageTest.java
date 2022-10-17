7087edcf390585c3f180b29e738fb020034c3837
unknown <hasib8nh@gmail.com>
2022-10-17 08:08:57 +0600

    Add product to the CartPage

---
+++ src/test/java/testcases/CartPageTest.java
@@ -0,0 +1,38 @@
+package testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import base.Base;
+import pageObject.CartPage;
+import pageObject.IndexPage;
+import pageObject.SearchResult;
+
+public class CartPageTest extends Base {
+	IndexPage indexPage;
+	SearchResult searchResult;
+	CartPage cartPage;
+	
+	
+	@BeforeMethod
+	public void setup() {
+	launchApp();
+	}
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	@Test
+	public void addToCartTest() throws Throwable {
+		indexPage = new IndexPage();
+		searchResult = indexPage.searchProduct("t-shirt");
+		cartPage = searchResult.clickOnProduct();
+		cartPage.enterQuantity("2");
+		cartPage.selectSize("M");
+		cartPage.clickOnAddToCart();
+		boolean result= cartPage.validateAddtoCart();
+		
+	}
+	
+}
