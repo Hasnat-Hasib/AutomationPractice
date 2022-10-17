e78e5e30992d5c19146ec591e05fd52caff1b64e
unknown <hasib8nh@gmail.com>
2022-10-17 07:46:02 +0600

    SearchResultTest run succesfullly

---
+++ src/test/java/testcases/SearchResultTest.java
@@ -0,0 +1,37 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import base.Base;
+import pageObject.IndexPage;
+import pageObject.LoginPage;
+import pageObject.SearchResult;
+
+public class SearchResultTest extends Base {
+	
+	IndexPage indexPage;
+	SearchResult searchResult;
+	
+	@BeforeMethod
+	public void setup() {
+	launchApp();
+	}
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void productAvailabilityTest() throws Throwable {
+		indexPage = new IndexPage();
+		searchResult = indexPage.searchProduct("t-shirt");
+		boolean result = searchResult.isProductAvailable();
+		Assert.assertTrue(result);
+		
+	}
+	
+
+}
